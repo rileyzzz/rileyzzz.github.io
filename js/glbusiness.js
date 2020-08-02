6362eec7b1d33092a2fdc66202587e06f81a12ee
Rileyzzz <rileylemmler@gmail.com>
2020-08-02 04:49:44 -0500

    renderer stuff

--- js/glbusiness.js
+++ js/glbusiness.js
@@ -1,3 +1,5 @@
+import { GLTFLoader } from './loaders/GLTFLoader.js';
+
 document.addEventListener('click', musicPlay);
 var context;
 var bufferLoader;
@@ -41,9 +43,42 @@ var renderer = new THREE.WebGLRenderer({ canvas: glCanvas });
 var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
+var grid;
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.y = 5;
+camera.lookAt(0, 0, 0);
+var loader = new GLTFLoader();
+
+loader.load('assets/3d/grid.glb', function (gltf) {
+
+	//gltf.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+	//grid = gltf.scene;
+	let uniforms = {
+		colorB: { type: 'vec3', value: new THREE.Color(0xACB6E5) },
+		colorA: { type: 'vec3', value: new THREE.Color(0x74ebd5) }
+	}
+	gltf.scene.traverse(function (child) {
+
+		if (child.isMesh) {
+
+			child.material = new THREE.ShaderMaterial({
+				uniforms: uniforms,
+				vertexShader: document.getElementById('vertexShader').textContent,
+				fragmentShader: document.getElementById('fragmentShader').textContent
+			});
+		}
+
+	});
+	grid = gltf.scene;
+	scene.add(grid);
+	//grid.rotation.x = Math.PI / 2;
+
+}, undefined, function (error) {
+
+	console.error(error);
+
+});
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
@@ -58,15 +93,17 @@ function onWindowResize() {
 var animate = function () {
 	requestAnimationFrame(animate);
 
-	cube.rotation.x += 0.01;
+	//cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-
+	//grid.rotation.x += 0.01;
+	grid.rotation.y += 0.01;
 	renderer.render(scene, camera);
 };
 
-animate();
+
 
 $(window).on('load', function () {
 	onWindowResize();
+	animate();
 });
 
