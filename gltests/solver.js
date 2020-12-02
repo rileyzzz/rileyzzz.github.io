f1d22ed9b0032c1a4c87a93d295ba01926dcac03
rileyzzz <rileylemmler@gmail.com>
2020-12-02 18:30:53 +0000

    more stuff

--- gltests/solver.js
+++ gltests/solver.js
@@ -2,9 +2,31 @@
 var possibilityMatrix = [];
 var possibilitySum = 0;
 
+function RemoveFrom(arr, val) {
+    let index = arr.indexOf(val);
+    if(index > -1)
+        arr.splice(index, 1);
+}
+
+function possibleAnd(A, B) {
+    var bitwise = 0;
+    for(let i = 0; i < A.length; i++)
+        bitwise |= 1 << (A[i] - 1);
+    alert(bitwise);
+    for(let i = 0; i < B.length; i++)
+        bitwise &= 1 << (B[i] - 1);
+    alert(bitwise);
+    var ret = [];
+    for(let i = 0; i < 9; i++)
+        if(bitwise & (1 << i))
+            ret.push(i + 1);
+    return ret;
+}
+
 function updatePossibilities() {
     var clears = [];
 
+    //alert("finding");
     for(let x = 0; x < 9; x++) {
         for(let y = 0; y < 9; y++) {
             let values = possibilityMatrix[x][y];
@@ -14,40 +36,68 @@ function updatePossibilities() {
         }
     }
 
-    for(clear in clears) {
+    //alert("clearing");
+    for(let i = 0; i < clears.length; i++) {
+        let clear = clears[i];
         let clearX = clear[0];
         let clearY = clear[1];
         let val = clear[2];
+        //alert("clear " + clearX + clearY + " " + val);
         for(let x = 0; x < 9; x++) {
-            if(x != clearX) {
-                var index = possibilityMatrix[x][clearY].indexOf(val);
-                if(index > -1)
-                    possibilityMatrix[x][clearY].splice(index, 1);
-            }
+            if(x != clearX)
+                RemoveFrom(possibilityMatrix[x][clearY], val);
         }
 
         for(let y = 0; y < 9; y++) {
-            if(y != clearY) {
-                var index = possibilityMatrix[clearX][y].indexOf(val);
-                if(index > -1)
-                    possibilityMatrix[clearX][y].splice(index, 1);
+            if(y != clearY)
+                RemoveFrom(possibilityMatrix[clearX][y], val);
+        }
+    }
+
+    for(let blockX = 0; blockX < 9; blockX += 3) {
+        for(let blockY = 0; blockY < 9; blockY += 3) {
+            let possible = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+            for(let x = blockX; x < blockX + 3; x++) {
+                for(let y = blockY; y < blockY + 3; y++) {
+                    let values = possibilityMatrix[x][y];
+                    if(values.length == 1)
+                        RemoveFrom(possible, values[0]);
+                }
+            }
+
+
+        }
+    }
+
+    //calculate sum
+
+    //alert("setting");
+    //update display
+    for(let x = 0; x < 9; x++) {
+        for(let y = 0; y < 9; y++) {
+            let values = possibilityMatrix[x][y];
+            if(values.length == 1) {
+                $("#n" + x + y).val(values[0]);
             }
         }
     }
-    //calculate sum
+    alert("done");
 }
 
 function compute() {
     //setup default property matrix
-    for(let x = 0; x < 9; x++) {
-        possibilityMatrix[x] = []
-        for(let y = 0; y < 9; y++) {
-            const possible = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-            var text = $("#n" + x + y).val();
-            if(text.value == "")
-                possibilityMatrix[x].push([...possible]);
-            else
-                possibilityMatrix[x].push([parseInt(text)]);
+    if(possibilityMatrix.length == 0) {
+        for(let x = 0; x < 9; x++) {
+            possibilityMatrix[x] = []
+            for(let y = 0; y < 9; y++) {
+                const possible = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+                var text = $("#n" + x + y).val();
+                if(text == "")
+                    possibilityMatrix[x].push([...possible]);
+                else
+                    possibilityMatrix[x].push([parseInt(text)]);
+            }
         }
     }
 
@@ -56,6 +106,9 @@ function compute() {
 
 $(document).ready(function(){
     //alert("awesome");
+
+    alert(possibleAnd([1, 2, 3, 4], [2, 3, 4, 5]));
+
     var contents = "";
     for(let x = 0; x < 9; x++) {
         contents += "<tr>";
