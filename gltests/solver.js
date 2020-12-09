b4e191a5903fba153c545f9a2329a1106cd679ba
rileyzzz <rileylemmler@gmail.com>
2020-12-09 16:14:13 +0000

    stuff and things

--- gltests/solver.js
+++ gltests/solver.js
@@ -2,6 +2,18 @@
 var possibilityMatrix = [];
 var possibilitySum = 0;
 
+function output(str) {
+    var formattedStr = String(str);
+    console.log(formattedStr);
+    var text = $("#output").val();
+    text += formattedStr + '\n';
+    $("#output").val(text);
+}
+
+window.onerror = function(message, source, lineno, colno, error) {
+    this.output("ERROR: " + message + " " + error + " from " + source + " line: " + lineno + " col: " + colno);
+};
+
 function RemoveFrom(arr, val) {
     let index = arr.indexOf(val);
     if(index > -1)
@@ -32,22 +44,23 @@ function updatePossibilities() {
     var clears = [];
 
     //alert("finding");
-    for(let x = 0; x < 9; x++) {
-        for(let y = 0; y < 9; y++) {
+    for(let y = 0; y < 9; x++) {
+        for(let x = 0; x < 9; y++) {
             let values = possibilityMatrix[x][y];
             if(values.length == 1) {
                 clears.push([x, y, values[0]]);
             }
         }
     }
-
+    output("CLEARS: " + clears);
     //alert("clearing");
     for(let i = 0; i < clears.length; i++) {
         let clear = clears[i];
         let clearX = clear[0];
         let clearY = clear[1];
         let val = clear[2];
-        console.log("clear " + clearX + " " + clearY + " " + val);
+
+        output("clear " + clearX + " " + clearY + " " + val);
         for(let x = 0; x < 9; x++) {
             if(x != clearX)
                 RemoveFrom(possibilityMatrix[x][clearY], val);
@@ -59,21 +72,21 @@ function updatePossibilities() {
         }
     }
 
-    for(let blockX = 0; blockX < 9; blockX += 3) {
-        for(let blockY = 0; blockY < 9; blockY += 3) {
+    for(let blockY = 0; blockY < 9; blockY += 3) {
+        for(let blockX = 0; blockX < 9; blockX += 3) {
             let possible = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-            for(let x = blockX; x < blockX + 3; x++) {
-                for(let y = blockY; y < blockY + 3; y++) {
+            for(let y = blockY; y < blockY + 3; y++) {
+                for(let x = blockX; x < blockX + 3; x++) {
                     let values = possibilityMatrix[x][y];
                     if(values.length == 1)
                         RemoveFrom(possible, values[0]);
                 }
             }
-            console.log("block " + blockX + " " + blockY + " possible " + possible + ":");
+            output("block " + blockX + " " + blockY + " possible " + possible + ":");
             //and with block possibilities
-            for(let x = blockX; x < blockX + 3; x++)
-                for(let y = blockY; y < blockY + 3; y++) {
+            for(let y = blockY; y < blockY + 3; y++)
+                for(let x = blockX; x < blockX + 3; x++) {
                     //console.log("before " + x + " " + y + " " + possibilityMatrix[x][y]);
                     possibilityMatrix[x][y] = possibleAnd(possibilityMatrix[x][y], possible);
                     //console.log("after " + x + " " + y + " " + possibilityMatrix[x][y]);
@@ -95,13 +108,14 @@ function updatePossibilities() {
             }
         }
     }
-    alert("done");
+    output("done");
+    //output(possibilityMatrix);
 }
 
 function compute() {
     //setup default property matrix
     if(possibilityMatrix.length == 0) {
-        alert("rebuilding possibility matrix");
+        output("rebuilding possibility matrix");
         for(let x = 0; x < 9; x++) {
             possibilityMatrix[x] = []
             for(let y = 0; y < 9; y++) {
@@ -114,14 +128,14 @@ function compute() {
             }
         }
     }
-
+    output("begin update");
     updatePossibilities();
 }
 
 $(document).ready(function(){
     //alert("awesome");
 
-    alert(possibleAnd([1, 2, 3], [3, 4, 5, 6, 7]));
+    //output(possibleAnd([1, 2, 3], [3, 4, 5, 6, 7]));
 
     var contents = "";
     for(let x = 0; x < 9; x++) {
